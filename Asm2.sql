289408c16952aae4fc68e28c38e711fbb512e713
nguyenkhanh08112003 <nguyenkhanh08112003@gmail.com>
2021-12-31 09:45:58 +0700

    31-12

---
+++ Asm2.sql
@@ -0,0 +1,99 @@
+﻿create database asm02
+go
+
+use asm02
+create table Hang(
+MaHang int NOT NULL primary key,
+TenHang nvarchar(40),
+DiaChi nvarchar(100),
+DienThoai int
+)
+
+insert into Hang values
+(123, N'ASUS', N'USA', 0981256),
+(124, N'DELL', N'USA', 0912571),
+(125, N'LENOVO', N'Malaysia', 09125125),
+(126, N'APPLE', N'USA', 0912512),
+(127, N'MSI', N'ThaiLand', 0912512),
+(128, N'ROG', N'USA', 09125125),
+(220, N'NOKIA', N'USA', 0912512),
+(221, N'SAMSUNG', N'Malaysia', 0912357),
+(222, N'OPPO', N'USA', 09123587),
+(223, N'XIAOMI', N'China', 0912457)
+
+
+create table Kho(
+MaSP varchar(40) primary key,
+TenSP nvarchar(100),
+MaHang int references Hang(MaHang),
+MoTa nvarchar(100),
+DonVi nvarchar (40),
+Gia int,
+SoLuong int
+)
+
+insert into Kho values 
+('123X2', N'LapTop', 123, N'Còn mới', N'Chiếc', 25000, 10),
+('123X3', N'LapTop', 123, N'Còn mới', N'Chiếc', 24000, 20),
+('124Y6', N'LapTop', 124, N'Còn mới', N'Chiếc', 25500, 11),
+('124Y7', N'LapTop', 124, N'Còn mới', N'Chiếc', 23300, 8),
+('125R8', N'LapTop', 125, N'Còn mới', N'Chiếc', 24000, 3),
+('125R9', N'Điện thoại', 125, N'Còn mới', N'Chiếc', 8000, 9),
+('126A10', N'Điện thoại', 126, N'Còn mới', N'Chiếc', 9500, 12),
+('126A13', N'Điện thoại', 126, N'Còn mới', N'Chiếc', 9500, 15),
+('127C9', N'Điện thoại', 127, N'Còn mới', N'Chiếc', 8500, 9999),
+('127C2', N'Điện thoại', 127, N'99%', N'Chiếc', 6500, 25),
+('127C12', N'LapTop', 127, N'99%', N'Chiếc', 16000, 9999),
+('220T1', N'LapTop', 220, N'99%', N'Chiếc', 15500, 12),
+('220T12', N'LapTop', 220, N'99%', N'Chiếc', 18000, 4),
+('220T13', N'Điện thoại', 220, N'99%', N'Chiếc', 5500, 9999)
+
+create table Loai(
+MaLoai varchar(40),
+LoaiSP nvarchar(100)
+)
+
+insert into Loai values 
+('X', N'Điện thoại'),
+('Y', N'LapTop')
+
+--Hiển thị tên các hãng 
+select TenHang from Hang
+
+--Hiển thị tên các sản phẩn bán
+select TenSP from Kho
+
+--Sắp xếp tên sản phẩm theo bảng chữ cái alphabet
+select TenSP from Kho order by TenSP ASC
+
+--Sắp xếp tên sản phẩm theo giá giảm dần
+select TenSP, Gia from Kho order by Gia DESC
+
+--Hiển thị thông tin của hãng ASUS
+select * from Hang where TenHang = 'ASUS'
+
+--Hiển thị các mặt hàng có số lượng hàng ít hơn 11 trong kho
+select * from Kho where SoLuong < 11
+
+--Hiển thị các sản phẩm của hãng ASUS
+select * from Kho where MaHang = 123
+
+--Đếm số hãng mà cửa hàng bán
+select count(MaHang) from Hang 
+
+--Đếm số mặt hàng có trong kho
+select count(TenSP) from Kho
+
+--Đếm số lượng sản phẩm của từng hãng
+select count(TenSP) from Kho where MaHang = '123' --Thay đổi mã hãng để đếm số lượng sản phẩm của các hãng còn lại
+
+--Đếm số đầu sản phẩm có bán tại cửa hàng
+select count (LoaiSP) from Loai
+
+--Cập nhật yêu cầu giá luôn dương (lớn hơn 0)
+alter table Kho 
+add constraint price check (Gia>0)
+
+--Cập nhật yêu cầu số điện thoại phải bắt đầu bằng số 0 -> chuyển kiểu dữ liệu sang nvarchar hoặc varchar
+alter table Hang
+alter column DienThoai nvarchar(100)jj
---
+++ Asm3.sql
@@ -0,0 +1,40 @@
+﻿create database asm3
+go
+
+use asm3
+create table ThongTin(
+TenKH nvarchar(100),
+SoCMT int primary key,
+DiaChi nvarchar(100)
+)
+
+insert into ThongTin values
+(N'Nguyễn Bá Khánh', 00123456789, N'Hà Nội'),
+(N'Đinh Quang Anh', 00123456788, N'Ninh Bình'),
+(N'Tạ Duy Linh', 0012345687, N'Thái Nguyên'),
+(N'Nguyễn Mạnh Kiên', 00123456786, N'Nhà Thầy'),
+(N'Tạ Văn Minh', 00123456785, N'Thanh Hóa'),
+(N'Lương Viết Hoàng', 00123456784, N'Hạ Long')
+
+create table GiayDK(
+TenKH nvarchar(100),
+SoCMT int references ThongTin(SoCMT),
+DiaChi nvarchar(100) references ThanhPho(DiaChi),
+ThueBao varchar(100) primary key,
+LoaiTB nvarchar(40),
+NgayDK date
+)
+
+insert into GiayDK values 
+(N'Nguyễn Bá Khánh', 00123456789, N'Hà Nội', '0987729543', N'Trả trước', '2021-12-31')
+(N'Nguyễn Bá Khánh', 00123456789, N'Hà Nội', '0392652354', N'Trả trước', '2021-12-31')
+(N'Tạ Duy Linh', 00123456787, N'Hà Nội', '0912531353', N'Trả trước', '2021-12-31')
+(N'Dinh Quang Anh', 00123456788, N'Hà Nội', '091242353', N'Trả trước', '2021-12-28')
+(N'Nguyễn Mạnh Kiên', 00123456786, N'Hà Nội', '092351543', N'Trả trước', '2021-12-30')
+(N'Vũ Viết Quý', 00123456785, N'Hà Nội', '012412453', N'Trả trước', '2021-12-30')
+(N'Lương Viết Hoàng', 00123456784, N'Hà Nội', '03512541231', N'Trả trước', '2021-12-26')
+
+create table ThanhPho(
+MaTP int,
+DiaChi nvarchar(100) primary key
+)
